e044f290bc229c65d13e07f6f29708f7a0b8f6a4
githubuiux <uiuxista@gmail.com>
2023-04-05 00:31:35 +0300

    Initial Commit

---
+++ Introanimation/IntroanimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroanimationApp.swift
+//  Introanimation
+//
+//  Created by Levent on 5.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct IntroanimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
